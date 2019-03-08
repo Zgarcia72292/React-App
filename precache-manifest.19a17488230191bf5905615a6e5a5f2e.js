f1dfd7ccafc1a9fd1adb44976a586f1f0d445d06
TheZShow <zgarcia72292@gmail.com>
2019-03-07 18:39:37 -0600

    Updates

---
+++ precache-manifest.19a17488230191bf5905615a6e5a5f2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed6dc382777155abadf9",
+    "url": "/React-App/static/js/runtime~main.ed6dc382.js"
+  },
+  {
+    "revision": "186ed806b3a201f358e1",
+    "url": "/React-App/static/js/main.186ed806.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/React-App/static/js/2.ff661de6.chunk.js"
+  },
+  {
+    "revision": "186ed806b3a201f358e1",
+    "url": "/React-App/static/css/main.d6710d31.chunk.css"
+  },
+  {
+    "revision": "2825bdd06c75175fe8c27f5575266bda",
+    "url": "/React-App/index.html"
+  }
+];
